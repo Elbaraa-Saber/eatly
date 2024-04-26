4001c375f669bc7ea2175c22504a7d6df675a914
Eblaraa <baraax01@gmail.com>
2024-04-26 10:52:53 +0700

    i added Features component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Navbar from './components/Navbar';
 import ReactDOM from 'react-dom'
 import Home from './components/Home';
+import Features from './components/Features';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Home/>
-
+      <Features/>
 
     </div>
   );
---
+++ src/components/Download_app.js
@@ -0,0 +1,3 @@
+function Download_app(){
+
+}
